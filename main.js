535971d52b34785818ff92c2706eb4700f36038e
GianniVaiente <43664060+GianniVaiente@users.noreply.github.com>
2018-10-01 12:50:55 -0700

    Made code DRY
    Refactored repeated code into a for loop

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var main = function(){
     "use strict";
-    var click = 0;
+    var checkForInput = true;
     var changeValue = function (x){
         if($("."+x).text()==""){
             $("."+x).text("0");
@@ -12,76 +12,13 @@ var main = function(){
             $("."+x).text("");
             click =0;
         }
-    }
-
-    $(".0").on("click" , function(event){
-        changeValue(0);
-        click ++
-    });
-    $(".1").on("click" , function(event){
-        changeValue(1);
-        click ++
-        
-    });
-    $(".2").on("click" , function(event){
-        changeValue(2);
-        click ++
-    });
-    $(".3").on("click" , function(event){
-        changeValue(3);
-        click ++
-    });
-    $(".4").on("click" , function(event){
-        changeValue(4);
-        click ++
-    });
-    $(".5").on("click" , function(event){
-        changeValue(5);
-        click ++
-    });
-    $(".6").on("click" , function(event){
-        changeValue(6);
-        click ++
-    });
-    $(".7").on("click" , function(event){
-        changeValue(7);
-        click ++
-    });
-    $(".8").on("click" , function(event){
-        changeValue(8);
-        click ++
-    });
-    $(".9").on("click" , function(event){
-        changeValue(9);
-        click ++
-    });
-    $(".10").on("click" , function(event){
-        changeValue(10);
-        click ++
-    });
-    $(".11").on("click" , function(event){
-        changeValue(11);
-        click ++
-    });
-    $(".12").on("click" , function(event){
-        changeValue(12);
-        click ++
-    });
-    $(".13").on("click" , function(event){
-        changeValue(13);
-        click ++
-    });
-    $(".14").on("click" , function(event){
-        changeValue(14);
-        click ++
-    });
-    $(".15").on("click" , function(event){
-        changeValue(15);
-        click ++
-    });
-    $("button").on("click" , function(event){
-        $("td").text("");
-});
+    }   
+         for(let y = 0 ; y < 16; y++){
+        $("."+y).on("click" , function(event){
+                    changeValue(y);
+                    click ++
+         });
+     }
 
 }
 $(document).ready(main);
